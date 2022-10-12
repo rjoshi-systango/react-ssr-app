2fe7bd1f3da40d10da17d274bb223159d2e26f63
rjoshi-systango <rjoshi@systango.com>
2022-10-12 16:59:11 +0530

    feature: add meta text in product

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
 
 export default function ProductPage() {
     const items = useSelector(state => state.items)
     
   return (
     <div>
+        <Helmet>
+          <title>{items ? items[0].name : 'mens shirt'}</title>
+          <meta name="description" content={items ? items[0].tag : 'shirt are available here'} />
+        </Helmet>
         <Link to='/'>back to home</Link>
         {items?.map((item, index) => {
             return <h6 key={`item-sx-${index}`}>{item.name}</h6>
